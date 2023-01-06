3de56dc98a53dd642edb0ac33000171898057ef9
立婷 <0225pisces@gmail.com>
2023-01-06 11:33:21 +0800

    重新定義路由

--- src/router.js
+++ src/router.js
@@ -3,16 +3,17 @@ import Home from './views/Home.vue'
 
 export default () => createRouter({
   history: createWebHistory(),
-  routes: [
+  routes: [    
     {
       path: '/',
-      name: 'Home',
-      component: Home,
-    },
-    {
-      path: '/login',
       name: 'Login',
       component: () => import('./views/Login.vue')
+    },
+    {
+      path: '/home',
+      name: 'Home',
+      component: Home,
+      meta: { requireAuth: true }, // 用來作為此頁是否需要權限驗證的設定
     },
   ]
 })
